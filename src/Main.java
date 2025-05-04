e32c2548c149ab73520fe6d060197022dc17bc7f
Aikokul Talantbekova <aikokul.talantbekova.ch@gmail.com>
2025-05-04 17:30:15 -0500

    Adding to Git2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
-    public static void main (String[] args){
-        System.out.println("Hello, Aikokul");
+    public static void main(String[] args) {
+     int myAge = 24;
+     int myHusbandAge = myAge +1;
+     System.out.println(myHusbandAge);
     }
 }
