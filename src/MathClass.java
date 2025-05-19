c45d7d3b8297cc36427bb69dcf62af91a6b4d825
Aikokul Talantbekova <aikokul.talantbekova.ch@gmail.com>
2025-05-19 18:08:14 -0500

    Merge remote-tracking branch 'origin/master'

--- src/MathClass.java
+++ src/MathClass.java
@@ -24,6 +24,7 @@ public class MathClass {
         System.out.println(eleventh);
         int twelve = (int)(Math.random()*100);
         System.out.println(twelve);
+        System.out.println(twelve);
 
 
 
