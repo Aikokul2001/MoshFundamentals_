83e7fc6835025253f5e12fea2a7ee40c34a8d17b
Aikokul Talantbekova <aikokul.talantbekova.ch@gmail.com>
2025-05-22 23:01:17 -0500

    Merge remote-tracking branch 'origin/master'

---
+++ src/ComparisonOperators.java
@@ -0,0 +1,13 @@
+public class ComparisonOperators {
+    public static void main(String[] args) {
+        int x = -598;
+        int y = 854;
+        System.out.println(x == y);  // boolean expression == equal
+        System.out.println(x != y); // != not equal, how the expressions shows
+        System.out.println(x < y);
+        System.out.println(x > y);
+        System.out.println(x <= y );
+        System.out.println(x >= y);
+
+    }
+}
---
+++ src/StringExample2.java
@@ -0,0 +1,17 @@
+public class StringExample2 {
+    public static void main(String[] args) {
+        String message = "Hello World";
+        System.out.println(message);
+        String message1 = "Hello World" + "!!";
+        System.out.println(message1);
+
+        String message2 = "Hello my dears";
+        System.out.println(message2.endsWith("lovely")); //true or false values
+
+        String message3 = "Hello my dears";
+        System.out.println(message3.startsWith("Hello"));
+
+
+    }
+
+}
